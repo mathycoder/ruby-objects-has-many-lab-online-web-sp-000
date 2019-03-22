da98042d1c01da276c8246db1917f8edb19c53a5
Adam S <adam.sarli@gmail.com>
2019-03-22 18:09:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song 
-  attr_accessor :name 
+  attr_accessor :name, :artist 
   @@all = [] 
   
   def initialize(name)
@@ -9,6 +9,14 @@ class Song
   
   def self.all 
     @@all 
+  end 
+  
+  def artist_name 
+    if @artist.name != nil 
+      @artist.name 
+    else 
+      nil
+    end 
   end 
   
 end 
